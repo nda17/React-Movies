a4891edac18cec3eb737e08ec36c30ba9021ed3b
nfdmitry <khv1702@gmail.com>
2023-05-22 03:19:25 +0300

    Editing files

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,28 +1,25 @@
 export function Footer() {
-    return (
-        <footer className='footer'>
-            <nav className='navbar navbar-container navbar-light bg-dark'>
-                <div className='container-fluid'>
-                    <p className='copyright'>
-                        © {new Date().getFullYear()}
-                        <a
-                            className='navbar-brand'
-                            href='https://github.com/nfdmitry'
-                        >
-                            <img
-                                className='copyright-image'
-                                src='/public/icons/github.svg'
-                                alt='Github'
-                                width='30'
-                                height='30'
-                            />
-                        </a>
-                    </p>
-                    <a className='navbar-brand text-info' href='#'>
-                        Movies
-                    </a>
-                </div>
-            </nav>
-        </footer>
-    );
+	return (
+		<footer className='footer'>
+			<nav className='navbar navbar-container navbar-light bg-dark'>
+				<div className='container-fluid'>
+					<p className='copyright'>
+						© {new Date().getFullYear()}
+						<a className='navbar-brand' href='https://github.com/nfdmitry'>
+							<img
+								className='copyright-image'
+								src='/public/icons/github.svg'
+								alt='Github'
+								width='30'
+								height='30'
+							/>
+						</a>
+					</p>
+					<a className='navbar-brand text-info' href='#'>
+						Movies
+					</a>
+				</div>
+			</nav>
+		</footer>
+	);
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,48 +1,45 @@
 export function Header() {
-    return (
-			<header className='header'>
-				<nav className='navbar navbar-header navbar-container navbar-light bg-dark'>
-					<div className='container-fluid container-fluid-content'>
-						<a className='navbar-brand text-info' href='#'>
-							<img
-								className='logo-image'
-								src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV9rfM3Zjk6OO7K13q06foymm89qcD1jAU7g&usqp=CAU'
-								alt='Movie icon'
-							/>
-							Movies
-						</a>
-						<div className='nav-menu'>
-							<div
-								className='collapse show-off'
-								id='navbarToggleExternalContent'
-							>
-								<div className='bg-dark p-4 padding-0'>
-									<a className='link active' aria-current='page' href='#'>
-										About
-									</a>
-									<a className='link' href='#'>
-										Repo
-									</a>
-								</div>
+	return (
+		<header className='header'>
+			<nav className='navbar navbar-header navbar-container navbar-light bg-dark'>
+				<div className='container-fluid container-fluid-content'>
+					<a className='navbar-brand text-info' href='#'>
+						<img
+							className='logo-image'
+							src='/public/icons/movie-camera.png'
+							alt='Movie icon'
+						/>
+						Movies
+					</a>
+					<div className='nav-menu'>
+						<div className='collapse show-off' id='navbarToggleExternalContent'>
+							<div className='bg-dark p-4 padding-0'>
+								<a className='link active' aria-current='page' href='#'>
+									About
+								</a>
+								<a className='link' href='#'>
+									Repo
+								</a>
 							</div>
-							<nav className='navbar padding-0 navbar-dark bg-dark'>
-								<div className='container-fluid container-fluid-nav-menu'>
-									<button
-										className='navbar-toggler'
-										type='button'
-										onClick={() =>
-											document
-												.getElementById('navbarToggleExternalContent')
-												.classList.toggle('show')
-										}
-									>
-										<span className='navbar-toggler-icon'></span>
-									</button>
-								</div>
-							</nav>
 						</div>
+						<nav className='navbar padding-0 navbar-dark bg-dark'>
+							<div className='container-fluid container-fluid-nav-menu'>
+								<button
+									className='navbar-toggler'
+									type='button'
+									onClick={() =>
+										document
+											.getElementById('navbarToggleExternalContent')
+											.classList.toggle('show')
+									}
+								>
+									<span className='navbar-toggler-icon'></span>
+								</button>
+							</div>
+						</nav>
 					</div>
-				</nav>
-			</header>
-		);
+				</div>
+			</nav>
+		</header>
+	);
 }
